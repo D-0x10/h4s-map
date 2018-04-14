ebe8c05a3d997826b291dd2be277d269e2558b20
Marcus Rettig <marcus0706@live.se>
2018-04-14 23:37:50 +0200

    Split json data

--- src/main.js
+++ src/main.js
@@ -15,6 +15,8 @@ if (window.location.hash) {
   }
 }
 
+var info = {};
+
 map.on('load', function() {
   map.addLayer({
     id: 'collisions',
@@ -27,14 +29,14 @@ map.on('load', function() {
       'circle-radius': [
         'interpolate',
         ['linear'],
-        ['number', ['get', 'involvedSize']],
+        ['number', ['get', 'inv']],
         1, 5,
         10, 20
       ],
       'circle-color': [
         'interpolate',
         ['linear'],
-        ['number', ['get', 'intensity']],
+        ['number', ['get', 'in']],
         0, '#ADB9C9',
         1, '#9DBF15',
         2, '#FF9947',
@@ -48,13 +50,16 @@ map.on('load', function() {
 
 map.on('click', 'collisions', function (e) {
   var props = e.features[0].properties;
-  document.getElementById('infoType').innerHTML = accidentTypes[props.accidentType];
-  document.getElementById('infoInvlovedCount').innerHTML = props.involvedSize;
-  document.getElementById('infoLightCondition').innerHTML = props.lightCondition;
-  document.getElementById('infoLocationType').innerHTML = locationTypes[props.locationType];
-  document.getElementById('infoRoadCondition').innerHTML = props.roadCondition;
-  document.getElementById('infoLightCondition').innerHTML = lightConditions[props.lightCondition];
-  document.getElementById('infoWeather').innerHTML = weathers[props.weather];
+  function p(key) {
+    return info[props.name][key];
+  }
+  document.getElementById('infoType').innerHTML = accidentTypes[p('accidentType')];
+  document.getElementById('infoInvlovedCount').innerHTML = props.inv;
+  document.getElementById('infoLightCondition').innerHTML = p('lightCondition');
+  document.getElementById('infoLocationType').innerHTML = locationTypes[p('locationType')];
+  document.getElementById('infoRoadCondition').innerHTML = p('roadCondition');
+  document.getElementById('infoLightCondition').innerHTML = lightConditions[p('lightCondition')];
+  document.getElementById('infoWeather').innerHTML = weathers[p('weather')];
 });
 
 slider.oninput = function() {
@@ -70,3 +75,18 @@ slider.oninput = function() {
   label.innerText = labels[month];
   map.setFilter('collisions', filters);
 }
+
+function httpGet(theUrl, callback)
+{
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.onreadystatechange = function() { 
+    if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+      callback(xmlHttp.responseText);
+  }
+  xmlHttp.open('GET', theUrl, true); // true for asynchronous 
+  xmlHttp.send();
+}
+
+httpGet('https://h4s-api.herokuapp.com/api/accidents/info/' + year, function(result) {
+  info = JSON.parse(result);
+});
